7c779965629a607497ec553bdae50ef0a054c5d2
Bastiaan Tiobook <deknudtb@gmail.com>
2021-05-29 12:45:52 +0200

    second format test

--- src/routes/todos/_api.ts
+++ src/routes/todos/_api.ts
@@ -23,33 +23,33 @@ export async function api(
   data?: Record<string, unknown>
 ): Promise<EndpointOutput> {
   // user must have a cookie set
-if (!request.locals.userid) {
-  return { status: 401 };
-}
+  if (!request.locals.userid) {
+    return { status: 401 };
+  }
 
-const res = await fetch(`${base}/${resource}`, {
-  method: request.method,
-  headers: {
-    'content-type': 'application/json',
-  },
-  body: data && JSON.stringify(data),
-});
-
-// if the request came from a <form> submission, the browser's default
-// behaviour is to show the URL corresponding to the form's "action"
-// attribute. in those cases, we want to redirect them back to the
-// /todos page, rather than showing the response
-if (res.ok && request.method !== 'GET' && request.headers.accept !== 'application/json') {
-  return {
-    status: 303,
+  const res = await fetch(`${base}/${resource}`, {
+    method: request.method,
     headers: {
-      location: '/todos',
+      'content-type': 'application/json',
     },
-  };
-}
+    body: data && JSON.stringify(data),
+  });
 
-return {
-  status: res.status,
-  body: await res.json(),
-};
+  // if the request came from a <form> submission, the browser's default
+  // behaviour is to show the URL corresponding to the form's "action"
+  // attribute. in those cases, we want to redirect them back to the
+  // /todos page, rather than showing the response
+  if (res.ok && request.method !== 'GET' && request.headers.accept !== 'application/json') {
+    return {
+      status: 303,
+      headers: {
+        location: '/todos',
+      },
+    };
+  }
+
+  return {
+    status: res.status,
+    body: await res.json(),
+  };
 }
