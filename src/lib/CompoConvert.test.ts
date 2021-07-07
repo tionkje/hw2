a135e7a6ac7dfa35e050ba3683bb8715202dd7d6
Bastiaan Tiobook <deknudtb@gmail.com>
2021-07-07 23:04:55 +0200

    convert compo support

--- src/lib/CompoConvert.test.ts
+++ src/lib/CompoConvert.test.ts
@@ -1,5 +1,7 @@
 import CompoConvert from './CompoConvert';
 
+const copy = (o) => JSON.parse(JSON.stringify(o));
+
 it('converts an empty competition', () => {
   function createEmptyOldData() {
     return {
@@ -14,4 +16,61 @@ it('converts an empty competition', () => {
 
   expect(oldCompo).toEqual(createEmptyOldData());
   expect(newCompo).toEqual({ categories: [], meters: [], name: '', throwers: [] });
+});
+
+it('converts a competition', () => {
+  const oldCompo = {
+    name: 'CompetitionName',
+    throwers: [
+      {
+        categoryid: 'Heren',
+        startHeight: 10,
+        hwId: 2,
+        name: 'Piet Callebaut',
+        nr: 4,
+        rugnr: 55,
+        attempts: {
+          10: ['S'],
+          10.5: ['S'],
+          11: ['X', 'V'],
+          11.5: ['V'],
+          12: ['X', 'X', 'X'],
+        },
+        finaleGrant: [12],
+        eliminated: 12.9,
+        rankNr: 2,
+      },
+    ],
+    categories: {
+      Heren: {
+        name: 'Heren',
+        id: 'Heren',
+        startHeight: 5,
+      },
+    },
+  };
+
+  const backup = copy(oldCompo);
+  const newCompo = copy(CompoConvert(oldCompo));
+  expect(oldCompo).toEqual(backup);
+  expect(newCompo).toEqual({
+    name: 'CompetitionName',
+    categories: [{ name: 'Heren' }],
+    throwers: [
+      {
+        name: 'Piet Callebaut',
+        skipHeight: 10.5,
+        categories: {
+          0: {
+            10: ['S'],
+            10.5: ['S'],
+            11: ['X', 'V'],
+            11.5: ['V'],
+            12: ['X', 'X', 'X'],
+          },
+        },
+      },
+    ],
+    meters: [],
+  });
 });
--- src/lib/CompoConvert.ts
+++ src/lib/CompoConvert.ts
@@ -1,9 +1,12 @@
-import { CompetitionData, Competition } from './Competition';
+import { CompetitionData, ThrowerData, Competition } from './Competition';
+import { strict as assert } from 'assert';
 
 type Height = number;
 type OldCategoryId = string;
-type OldJudge = 'X' | 'V' | 'S';
-type OldAttempts = [OldJudge, OldJudge, OldJudge];
+// type OldJudge = 'X' | 'V' | 'S';
+type OldJudge = string;
+// type OldAttempts = [OldJudge, OldJudge?, OldJudge?];
+type OldAttempts = OldJudge[];
 
 type OldThrowerData = {
   name: string;
@@ -13,8 +16,8 @@ type OldThrowerData = {
   nr: number;
   rugnr: number;
   attempts: Record<Height, OldAttempts>;
-  skipHeight: Height;
-  eliminated: Height;
+  skipHeight?: Height;
+  eliminated?: Height;
   rankNr: number;
 };
 
@@ -33,6 +36,33 @@ type OldCompetitionData = {
 export default function CompoConvert(old: OldCompetitionData): Competition {
   const compo: CompetitionData = {};
   compo.name = old.name;
+
+  const catIdMap = {};
+  compo.categories = [];
+  Object.keys(old.categories).forEach((catId) => {
+    const cat = old.categories[catId];
+    assert.equal(catId, cat.id);
+    const id = compo.categories.push({ name: cat.name }) - 1;
+    catIdMap[cat.id] = id;
+  });
+
+  compo.throwers = [];
+  old.throwers.forEach((oldThrower) => {
+    const thrower: ThrowerData = {};
+    thrower.name = oldThrower.name;
+
+    const heights = Object.keys(oldThrower.attempts)
+      .map((x) => Number(x))
+      .filter((h) => oldThrower.attempts[h].includes('S'))
+      .sort((a, b) => b - a);
+    thrower.skipHeight = Math.max(Number(heights[0] || 0), oldThrower.startHeight);
+
+    thrower.categories = {};
+    const catId = catIdMap[oldThrower.categoryid];
+    thrower.categories[catId] = oldThrower.attempts;
+
+    compo.throwers.push(thrower);
+  });
 
   return new Competition(compo);
 }
