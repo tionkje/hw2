8e089e431cc875642558af231101ebd32362989b
Bastiaan Tiobook <deknudtb@gmail.com>
2021-08-02 21:25:14 +0200

    added editmeters modal

---
+++ src/lib/EditMeters.svelte
@@ -0,0 +1,34 @@
+<script lang="ts">
+  import { createEventDispatcher } from 'svelte';
+  export let compo;
+
+  const dispatch = createEventDispatcher();
+  function close() {
+    dispatch('close');
+  }
+</script>
+
+<main>
+  <h1>Edit meters</h1>
+  <!-- <form on:submit|preventDefault={submitName}> -->
+  <!--   <input type="text" name="name" bind:value={name} /> -->
+  <!-- </form> -->
+
+  <pre>{JSON.stringify(compo, 0,2)}</pre>
+
+  <button on:click={close}>Close</button>
+</main>
+
+<style>
+  pre {
+    max-height: 50vh;
+    overflow: auto;
+  }
+  main {
+    background: white;
+    max-width: 1024px;
+    margin: 10px auto;
+    border-radius: 20px;
+    padding: 10px;
+  }
+</style>
