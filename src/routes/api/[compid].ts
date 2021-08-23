c902d43f7b7451e4e0aa1d8cf8b8c281eec63cc7
Bastiaan Tiobook <deknudtb@gmail.com>
2021-08-23 23:09:23 +0200

    removed pusher

--- src/routes/api/[compid].ts
+++ src/routes/api/[compid].ts
@@ -1,10 +1,6 @@
 import type { EndpointOutput, Request } from '@sveltejs/kit';
 import { getCompetition, deleteCompetition, processCompo } from '$lib/CompetitionDB';
 
-// import Pusher from 'pusher';
-// import { PUSHER_KEY, PUSHER_CLUSTER } from '$lib/Env';
-// const { PUSHER_SECRET, PUSHER_APPID } = process.env;
-
 import Ably from 'ably';
 import { ABLY_CHANNEL } from '$lib/Env';
 const { ABLY_API_KEY } = process.env;
@@ -21,14 +17,6 @@ export async function del({ params, locals }): Promise<EndpointOutput> {
   };
 }
 
-// const pusher = new Pusher({
-//   appId: PUSHER_APPID,
-//   key: PUSHER_KEY,
-//   secret: PUSHER_SECRET,
-//   cluster: PUSHER_CLUSTER,
-//   useTLS: true,
-// });
-
 export async function put({ params, body, locals }: Request): Promise<EndpointOutput> {
   if (!locals.loggedin) return { status: 401 };
   body = JSON.parse(body);
@@ -41,8 +29,6 @@ export async function put({ params, body, locals }: Request): Promise<EndpointOu
 
   const channel = ably.channels.get(ABLY_CHANNEL);
   channel.publish(`compo_${compid}`, newCompo);
-
-  // pusher.trigger(`compo_${compid}`, 'full', newCompo);
 
   return {
     body: newCompo,
